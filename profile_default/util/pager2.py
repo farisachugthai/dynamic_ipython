d4ff8491fc784c1b985b5deff1194221b58ff512
farisachugthai <farischugthai@gmail.com>
2019-07-27 22:56:12 -0400

    Update modules docs

--- profile_default/util/pager2.py
+++ profile_default/util/pager2.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-"""Because John organizes his code in the most confusing way possible.
+"""Rewrite how IPython implements pagers on Windows.
+
+So would it be easier to do this as a magic or using Traitlets?
 
 """
 import sys
@@ -8,19 +10,42 @@ from pygments.lexers.python import PythonLexer
 
 from IPython import get_ipython
 # Might need some of the funcs from IPython.utils.{PyColorize,coloransi,colorable}
-from IPython.core.magic import Magics, magics_class, line_magic
 from IPython.core.error import UsageError
+from IPython.core.magic import Magics, magics_class, line_magic
+from IPython.core.magic_arguments import (argument, magic_arguments,
+                                          parse_argstring)
 
+
+def lpad_ipython_docstring(shell):
+    """TODO: Docstring for lpad_ipython_docstring.
+
+    Parameters
+    ----------
+    shell : |ip|
+        Global IPython object.
+
+    Returns
+    -------
+    TODO
+
+    """
+    shell.register_magics(MyMagics)
+
+
+@magic_arguments
 def main():
     """Rewrite the module that creates the ``%pycat`` magic.
 
     In it's current implementation, the pager gives Windows a dumb terminal and
-    never hcecks for whether :command:`less` is on the $PAN or
-    ekkkkkkkkkkkkkk]]]]]]]]]]]]]]]]]]]]]]]]                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+    never checks for whether :command:`less` is on the :envvar:`PATH` or
+    if the user has a pager they wanna implement!
+
     :returns: TODO
 
     """
     pass
 
+
 if __name__ == "__main__":
+    _ip = get_ipython()
     main()
