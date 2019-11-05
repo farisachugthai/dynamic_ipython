0213cde7e1e6e03f08ac866bccd6f217a4754673
farisachugthai <farischugthai@gmail.com>
2019-11-04 19:34:18 -0500

    Delete unused templates and readline

--- default_profile/startup/30_readline.py
+++ default_profile/startup/30_readline.py
@@ -9,9 +9,37 @@ seem to work just fine.
 Ooo also I want to reimplement jedi as the completer because IPython's is
 confusingly slow.
 """
+import logging
+import os
+from pathlib import Path
+
+
+def readline_logging():
+    if os.environ.get('IPYTHONDIR'):
+        LOG_FILENAME = os.path.join(os.environ.get('IPYTHONDIR'), 'completer.log')
+    # else:
+        # todo
+
+    logging.basicConfig(
+        format='%(message)s',
+        filename=LOG_FILENAME,
+        level=logging.DEBUG,
+    )
+
+# I want this executing before anything.
+
 try:
     import jedi
 except (ImportError, ModuleNotFoundError):
+    pass
+else:
+    from jedi.utils import setup_readline
+    setup_readline()
+    jedi.settings.add_bracket_after_function = True
+    # jedi.settings
+
+
+def get_readline():
     # Fallback to the stdlib readline completer if it is installed.
     # Taken from http://docs.python.org/2/library/rlcompleter.html
     try:
@@ -21,15 +49,84 @@ except (ImportError, ModuleNotFoundError):
         try:
             import readline
         except (ImportError, ModuleNotFoundError):
-            pass
+            readline = None
         else:
             readline.parse_and_bind("tab: complete")
+            return readline
     else:
         readline.parse_and_bind("tab: complete")
+        return readline
 
-        import rlcompleter
-else:
-    from jedi.utils import setup_readline
-    setup_readline()
-    jedi.settings.add_bracket_after_function = True
-    jedi.settings
+readline_mod = get_readline()
+
+def read_inputrc():
+    """Check for an inputrc file."""
+    if os.environ.get('INPUTRC'):
+        readline.read_init_file(os.environ.get('INPUTRC'))
+
+
+class SimpleCompleter:
+    """
+    :URL: https://pymotw.com/3/readline/
+    The SimpleCompleter class keeps a list of “options” that are candidates for auto-
+    completion. The complete() method for an instance is designed to be registered with
+    readline as the source of completions. The arguments are a text string to complete
+    and a state value, indicating how many times the function has been called with the
+    same text. The function is called repeatedly with the state incremented each time. It
+    should return a string if there is a candidate for that state value or None if there
+    are no more candidates. The implementation of complete() here looks for a set of
+    matches when state is 0, and then returns all of the candidate matches one at a time
+    on subsequent calls.
+    """
+
+    def __init__(self, options):
+        self.options = sorted(options)
+
+    def complete(self, text, state):
+        response = None
+        if state == 0:
+            # This is the first time for this text,
+            # so build a match list.
+            if text:
+                self.matches = [
+                    s
+                    for s in self.options
+                    if s and s.startswith(text)
+                ]
+                logging.debug('%s matches: %s',
+                              repr(text), self.matches)
+            else:
+                self.matches = self.options[:]
+                logging.debug('(empty input) matches: %s',
+                              self.matches)
+
+        # Return the state'th item from the match list,
+        # if we have that many.
+        try:
+            response = self.matches[state]
+        except IndexError:
+            response = None
+        logging.debug('complete(%s, %s) => %s',
+                      repr(text), state, repr(response))
+        return response
+
+
+def input_loop():
+    line = ''
+    while line != 'stop':
+        line = input('Prompt ("stop" to quit): ')
+        print('Dispatch {}'.format(line))
+
+
+# Register the completer function
+# OPTIONS = ['start', 'stop', 'list', 'print']
+# readline.set_completer(SimpleCompleter(OPTIONS).complete)
+
+
+if __name__ == "__main__":
+# Oddly enough this is available on every platform
+    import rlcompleter
+
+    if hasattr(readline, 'read_init_file'):
+        read_inputrc()
+
