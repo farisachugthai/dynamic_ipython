dbcc412dada837ca7e17af98457edc939da6be73
farisachugthai <farischugthai@gmail.com>
2020-01-09 03:18:30 -0500

    Rewrite the tests

---
+++ test/__main__.py
@@ -0,0 +1,71 @@
+import doctest
+import importlib
+import sys
+import unittest
+from unittest.suite import TestSuite
+from unittest.case import TestCase
+
+from IPython.testing.tools import get_ipython_cmd
+
+def _test():
+    import argparse
+
+    parser = argparse.ArgumentParser(description="doctest runner")
+    parser.add_argument('-v', '--verbose', action='store_true', default=False,
+                        help='print very verbose output for all tests')
+    parser.add_argument('-o', '--option', action='append',
+                        choices=doctest.OPTIONFLAGS_BY_NAME.keys(), default=[],
+                        help=('specify a doctest option flag to apply'
+                              ' to the test run; may be specified more'
+                              ' than once to apply multiple options'))
+    parser.add_argument('-f', '--fail-fast', action='store_true',
+                        help=('stop running tests after first failure (this'
+                              ' is a shorthand for -o FAIL_FAST, and is'
+                              ' in addition to any other -o options)'))
+    parser.add_argument('file', nargs='+',
+                        help='file containing the tests to run')
+    args = parser.parse_args()
+    testfiles = args.file
+    # Verbose used to be handled by the "inspect argv" magic in DocTestRunner,
+    # but since we are using argparse we are passing it manually now.
+    verbose = args.verbose
+    options = 0
+    for option in args.option:
+        options |= doctest.OPTIONFLAGS_BY_NAME[option]
+    if args.fail_fast:
+        options |= doctest.FAIL_FAST
+    for filename in testfiles:
+        if filename.endswith(".py"):
+            # It is a module -- insert its dir into sys.path and try to
+            # import it. If it is part of a package, that possibly
+            # won't work because of package imports.
+            dirname, filename = os.path.split(filename)
+            sys.path.insert(0, dirname)
+            m = __import__(filename[:-3])
+            del sys.path[0]
+            failures, _ = testmod(m, verbose=verbose, optionflags=options)
+        else:
+            failures, _ = testfile(filename, module_relative=False,
+                                     verbose=verbose, optionflags=options)
+        if failures:
+            return 1
+    return 0
+
+
+if __name__ == "__main__":
+    # Believe it or not this is in fact necessary
+    old_sys_argv = sys.argv[:]
+    # DONT FORGET SPACES
+    # sys.argv = [sys.executable, ' -m', ' unittest', ' -v']
+    sys.argv = get_ipython_cmd(as_string=False)
+    # todo: add options
+    test_00_ipython = importlib.import_module('test_00_ipython', package='.')
+    test_20_aliases = importlib.import_module('test_20_aliases', package='.')
+
+    suite = TestSuite()
+    suite.addTest(test_00_ipython.TestIPython())
+    suite.addTest(test_20_aliases.TestAliases())
+    unittest.main()
+
+    _test()
+    # sys.argv = old_sys_argv
--- test/test_00_ipython.py
+++ test/test_00_ipython.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python3
 """Arguably the best canary in the coal mine that something's wrong with setup.
+
+Lmao also dude check out what doctest has at the bottom.
 """
+import doctest
 import logging
 import unittest
+import shutil
+import tempfile
+import unittest
+
+
+def remove_tmpdir(dirname):
+    print('In remove_tmpdir()')
+    shutil.rmtree(dirname)
+
 
 import IPython
-from IPython import get_ipython
+from IPython.core.getipython import get_ipython
 
 
 class TestIPython(unittest.TestCase):
@@ -16,15 +28,6 @@ class TestIPython(unittest.TestCase):
     """
 
     shell = get_ipython()
-
-    def __init__(self, shell=None):
-        super().__init__()
-        if shell is not None:
-            self.shell = shell
-        else:
-            from IPython import start_ipython
-
-            self.shell = start_ipython()
 
     def test_ipython(self):
         """Produces unexpected results. TODO."""
@@ -43,6 +46,120 @@ class TestIPython(unittest.TestCase):
         methods.
         """
         return self.test_ipython()
+
+
+######################################################################
+## 9. Example Usage
+######################################################################
+
+
+class _TestClass:
+    """
+    A pointless class, for sanity-checking of docstring testing.
+
+    Methods:
+        square()
+        get()
+
+    >>> _TestClass(13).get() + _TestClass(-12).get()
+    1
+    >>> hex(_TestClass(13).square().get())
+    '0xa9'
+    """
+
+    def __init__(self, val):
+        """val -> _TestClass object with associated value val.
+
+        >>> t = _TestClass(123)
+        >>> print(t.get())
+        123
+        """
+
+        self.val = val
+
+    def square(self):
+        """square() -> square TestClass's associated value
+
+        >>> _TestClass(13).square().get()
+        169
+        """
+
+        self.val = self.val ** 2
+        return self
+
+    def get(self):
+        """get() -> return TestClass's associated value.
+
+        >>> x = _TestClass(-42)
+        >>> print(x.get())
+        -42
+        """
+
+        return self.val
+
+__test__ = {"_TestClass": _TestClass,
+            "string": r"""
+                      Example of a string object, searched as-is.
+                      >>> x = 1; y = 2
+                      >>> x + y, x * y
+                      (3, 2)
+                      """,
+
+            "bool-int equivalence": r"""
+                                    In 2.2, boolean expressions displayed
+                                    0 or 1.  By default, we still accept
+                                    them.  This can be disabled by passing
+                                    DONT_ACCEPT_TRUE_FOR_1 to the new
+                                    optionflags argument.
+                                    >>> 4 == 4
+                                    1
+                                    >>> 4 == 4
+                                    True
+                                    >>> 4 > 4
+                                    0
+                                    >>> 4 > 4
+                                    False
+                                    """,
+
+            "blank lines": r"""
+                Blank lines can be marked with <BLANKLINE>:
+                    >>> print('foo\n\nbar\n')
+                    foo
+                    <BLANKLINE>
+                    bar
+                    <BLANKLINE>
+            """,
+
+            "ellipsis": r"""
+                If the ellipsis flag is used, then '...' can be used to
+                elide substrings in the desired output:
+                    >>> print(list(range(1000))) #doctest: +ELLIPSIS
+                    [0, 1, 2, ..., 999]
+            """,
+
+            "whitespace normalization": r"""
+                If the whitespace normalization flag is used, then
+                differences in whitespace are ignored.
+                    >>> print(list(range(30))) #doctest: +NORMALIZE_WHITESPACE
+                    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
+                     15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
+                     27, 28, 29]
+            """,
+           }
+
+
+class FixturesTest(unittest.TestCase):
+
+    def setUp(self):
+        super().setUp()
+        self.tmpdir = tempfile.mkdtemp()
+        self.addCleanup(remove_tmpdir, self.tmpdir)
+
+    def test1(self):
+        print('\nIn test1()')
+
+    def test2(self):
+        print('\nIn test2()')
 
 
 if __name__ == "__main__":
