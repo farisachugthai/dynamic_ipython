0889b892eb703024d9bb574de8dd2cfbb2c5a841
farisachugthai <farischugthai@gmail.com>
2020-01-24 02:32:02 -0500

    Clearly cruft out and gitattributes

--- .dir-locals.el
+++
@@ -1,5 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((python/init-pyenv-mode
-  (pyvenv-default-virtual-env-name . "$HOME/miniconda3/envs/ipython")))
