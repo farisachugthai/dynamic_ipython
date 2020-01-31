c2a6c296669019ab2982cb784eda7c29d13aed2c
farisachugthai <farischugthai@gmail.com>
2020-01-31 09:48:34 -0500

    Update the debugger and add a lot of interactive tools

--- default_profile/profile_debugger/pdbrc.py
+++ default_profile/profile_debugger/pdbrc.py
@@ -10,23 +10,40 @@ Jesus Christ this got out of control.
 
 """
 import atexit
+from bdb import BdbQuit, Breakpoint
 import cgitb
 from contextlib import suppress
 import faulthandler
-import logging
-import os
+import gc
+import inspect
+import keyword
+from logging import getLogger, StreamHandler, BufferingFormatter, Filter
 from pathlib import Path
 import pdb
+import pydoc
+import rlcompleter
 import runpy
 import sys
 import time
 import trace
+import traceback
 
 # Run all this before any non-std lib imports. They should get profiled too
 print(f".pdbrc.py started {time.ctime()}")
 
+log_format = (
+    "[ %(name)s : %(relativeCreated)d :] %(levelname)s : %(module)s : --- %(message)s "
+)
 start = time.time()
-logger = logging.getLogger(name=__name__)
+
+logger = getLogger(name=__name__)
+handler = StreamHandler()
+formatter = BufferingFormatter(linefmt=log_format)
+handler.setFormatter(formatter)
+logger.addFilter(logging.Filter())
+logger.addHandler(handler)
+logger.setLevel(30)
+
 faulthandler.enable()
 cgitb.enable(format="text")
 
@@ -34,6 +51,12 @@ try:
     from prompt_toolkit.shortcuts import print_formatted_text as print
 except:  # noqa
     from pprint import pprint as print
+    debugger_completer = rlcompleter.Completer.complete
+else:
+    from prompt_toolkit.completion.fuzzy_completer import FuzzyWordCompleter
+    debugger_completer = FuzzyWordCompleter(words=keyword.kwlist).get_completions
+
+
 try:
     from IPython.core.getipython import get_ipython
 except:  # noqa
@@ -47,12 +70,16 @@ with suppress(ImportError):
     from IPython.lib.pretty import pprint
 
 
+with suppress(ImportError):
+    from jedi.api import replstartup
+
 try:
     import readline
 except:
     pass
-else:
+else:# this makes completions look classic IPython style
     readline.parse_and_bind("Tab:menu-complete")
+
 # I have a really useful module for importing readline on windows, linux,
 # WSL, and anything else you can imagine. let's use it.
 try:
@@ -74,7 +101,7 @@ else:
 
 
 def save_history(hist_path=None):
-    if not readline:
+    if not hasattr(locals, 'readline'):
         return
     if not hasattr(readline, "append_history_file"):
         print("ERR: no append_history_file method in readline")
@@ -84,6 +111,7 @@ def save_history(hist_path=None):
         if not hist_path.exists():
             logging.warning("Creating PDB history file at ~/.pdb_history.")
             hist_path.touch()
+        hist_path = hist_path.__fspath__()
 
     readline.append_history_file(hist_path)
 
@@ -103,18 +131,34 @@ else:
 
     readline_mod.setup_historyfile("~/.pdb_history")
 
+# Yes I'm still importing stuff
+
+try:
+    import pygments
+except ImportError:
+    colorizer = None
+else:
+    from pygments.lexers.python import PythonLexer
+    from pygments.formatters.terminal256 import TerminalTrueColorFormatter
+
+    lexer = PythonLexer()
+    formatter = TerminalTrueColorFormatter()
+
+
+    def colorizer(code):
+        return pygments.highlight(code, lexer, formatter)
 
 # Customized Pdb
 
 
 class MyPdb(pdb.Pdb):
-    """Subclass pdb.Pdb. Goddamn the only kw it uses that I wrote was skip."""
-
+    """Subclass Pdb."""
     prompt = f"<YourPdb> : "
     doc_header = ""
 
     def __init__(
         self,
+        completekey='tab',
         skip="traitlets",
         prompt=None,
         doc_header=None,
@@ -122,26 +166,56 @@ class MyPdb(pdb.Pdb):
         *args,
         **kwargs,
     ):
+        """
+        To explain all the keyword arguments, pdb inherits from both
+        :class:`bdb.Bdb` and :class:`cmd.Cmd`. So pydoc lists a lot
+        of attributes on `Pdb` that don't exist on `Bdb`.
+
+        .. todo:: I keep getting errors about self.botframe not being defined?
+        """
         self.skip = skip
         self.prompt = prompt
         self.doc_header = doc_header
         self.shell = shell
         if self.shell is not None:
             self.prompt += " [" + self.shell.execution_count + "]: "
-        super().__init__(skip=self.skip, *args, **kwargs)
+        self.bp = Breakpoint
+        self.completekey = completekey
+
+        super().__init__(completekey=self.completekey, skip=self.skip, *args, **kwargs)
 
     def __repr__(self):
-        return repr(self.__class__.__name__)
+        """Better repr with :meth:`bdb.Bdb.get_all_breaks` thrown in."""
+        return f"<{self.__class__.__name__} {self.get_all_breaks()}"
 
-    def run(self):
-        # This is the important one
-        super().run()
+    def run(self, statement, *args, **kwargs):
+        """Garbage collect after running because asyncio."""
+        try:
+            super().run(statement=statement, *args, **kwargs)
+        except BdbQuit:
+            gc.collect()
+            return
+
+    def __call__(self, statement):
+        return self.run(statement)
+
+    def completedefault(self):
+        """The superclass has this return nothing. Lets have this complete `locals.keys()`"""
+        self.complete(sorted(locals().keys()))
+
+    def message(self, msg):
+        if colorizer is not None:
+            return colorizer(super().message(msg))
+
+    @staticmethod
+    def help():
+        # Ill concede this has nothing to do with anything but I find it helpful
+        pydoc.pipepager(inspect.getsource(pdb.Pdb), os.environ.get('PAGER'))
 
 
 debugger = MyPdb()
 # TODO:
 # get_ipython().debugger_cls = MyPdb
-
 # Customize the sys.excepthook
 
 
@@ -159,4 +233,4 @@ sys.excepthook = exception_hook
 
 end = time.time()
 duration = end - start
-print(".pdbrc.py finished.{time.ctime()}\nduration was: {duration}.\n")
+print(f".pdbrc.py finished.{time.ctime()}\nduration was: {duration}.\n")
